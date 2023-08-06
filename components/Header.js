d65b4dddab489a3c0008e736a62e4557be070d02
coding-to-music <connors.tom@gmail.com>
2023-08-05 23:27:45 -0500

    Build

--- components/Header.js
+++ components/Header.js
@@ -16,6 +16,16 @@ const Header = () => {
               <a>Avatars</a>
             </Link>
           </li>
+          <li>
+            <Link href="/api">
+              <a>API</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/about">
+              <a>About</a>
+            </Link>
+          </li>
         </ul>
       </nav>
       <style jsx>{`
