b6e766c46586858a6aa18c5841202e93716e5c1e
coding-to-music <connors.tom@gmail.com>
2023-08-05 21:08:52 -0500

    Build

--- pages/characters/[characterId].js
+++ pages/characters/[characterId].js
@@ -1,36 +1,48 @@
+import Image from "next/image";
+
 export default function Character({ character }) {
   return (
     <div>
-      <img src={character.photoUrl} alt="" />
+      <Image src={character.photoUrl} alt="" />
       <div>
-        <h1>{ character.name }</h1>
-        <p>Affiliation: { character.affiliation }</p>
+        <h1>{character.name}</h1>
+        <p>Affiliation: {character.affiliation}</p>
       </div>
     </div>
-  )
+  );
 }
 
 export async function getStaticProps({ params }) {
-  const characterId = params.characterId.replace(/\-/g, '+')
-  const results = await fetch(`https://last-airbender-api.herokuapp.com/api/v1/characters?name=${characterId}`).then(res => res.json());
+  const characterId = params.characterId.replace(/\-/g, "+");
+  const results = await fetch(
+    `https://last-airbender-api.herokuapp.com/api/v1/characters?name=${characterId}`
+  ).then((res) => res.json());
   return {
     props: {
-      character: results[0] || {}
-    }
-  }
+      character: results[0] || {},
+    },
+  };
 }
 
 export async function getStaticPaths() {
-  const characters = await fetch('https://last-airbender-api.herokuapp.com/api/v1/characters?perPage=500').then(res => res.json());
+  const characters = await fetch(
+    "https://last-airbender-api.herokuapp.com/api/v1/characters?perPage=500"
+  ).then((res) => res.json());
   return {
-    paths: characters.map(character => {
-      const characterId = character.name.toLowerCase().replace(/ /g, '-').replace(/\'/, '').replace(/\(.+\)/, '');
-      return {
-        params: {
-          characterId
-        }
-      }
-    }).filter(({ params }) => !!params.characterId),
-    fallback: false
-  }
+    paths: characters
+      .map((character) => {
+        const characterId = character.name
+          .toLowerCase()
+          .replace(/ /g, "-")
+          .replace(/\'/, "")
+          .replace(/\(.+\)/, "");
+        return {
+          params: {
+            characterId,
+          },
+        };
+      })
+      .filter(({ params }) => !!params.characterId),
+    fallback: false,
+  };
 }
