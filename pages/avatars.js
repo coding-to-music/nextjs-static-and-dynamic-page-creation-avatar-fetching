5a216c38e04c8e6c5481861ef7296b1f35997bda
coding-to-music <connors.tom@gmail.com>
2023-08-06 22:47:00 -0500

    Build

--- pages/avatars.js
+++ pages/avatars.js
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Header from "../components/Header";
+import styles from "../styles/Home.module.css";
 
 export default function Avatars({ avatars }) {
   return (
@@ -7,13 +8,8 @@ export default function Avatars({ avatars }) {
       <Header />
       <h1>Avatars</h1>
       <p>All of the Avatars that appeared in the show:</p>
-      <ul
-        style={{
-          display: "grid",
-          gridTemplateColumns: "1fr 1fr 1fr",
-          gridGap: "4em",
-        }}
-      >
+      {/* Apply the gridContainer class to the ul element */}
+      <ul className={styles.gridContainer}>
         {avatars.map((avatar) => {
           return (
             <li key={avatar._id}>
@@ -42,3 +38,34 @@ export async function getStaticProps() {
     },
   };
 }
+
+// export default function Avatars({ avatars }) {
+//   return (
+//     <div>
+//       <Header />
+//       <h1>Avatars</h1>
+//       <p>All of the Avatars that appeared in the show:</p>
+//       <ul
+//         style={{
+//           display: "grid",
+//           gridTemplateColumns: "1fr 1fr 1fr",
+//           gridGap: "4em",
+//         }}
+//       >
+//         {avatars.map((avatar) => {
+//           return (
+//             <li key={avatar._id}>
+//               <p>{avatar.name}</p>
+//               <Image
+//                 src={avatar.photoUrl}
+//                 alt="Avatar"
+//                 width={300}
+//                 height={300}
+//               />
+//             </li>
+//           );
+//         })}
+//       </ul>
+//     </div>
+//   );
+// }
